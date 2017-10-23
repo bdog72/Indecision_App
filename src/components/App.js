1ce36987b7247e7a5af3e42dbb918eb5f710610c
Brian Bycynski <high5bri@hotmail.com>
2017-10-23 09:14:26 -0400

    still working on it

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
-
+import CountryGame from './CountryGame'
+import worldImg from './world.jpg'
+import '../styles/app.scss'
 class App extends Component {
   render () {
-    return <div>
-      <h1>Hello BdOg</h1>
-    </div>
+    return (
+      <div className='flag-app'>
+        <header
+          className='title-header'
+          style={{ backgroundImage: `url(${worldImg})` }}>
+          <h1 className='title-text'>Guess The Flag</h1>
+        </header>
+        <CountryGame />
+      </div>
+    )
   }
 }
 
